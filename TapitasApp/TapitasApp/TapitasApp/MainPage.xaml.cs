af01242eae1f1bd1f4981627ed5e9dc29f8731d2
Daniel Mateo Guerra Montoya <dmguerra@correo.iue.edu.co>
2022-11-04 11:52:45 -0500

    Agregar archivos de proyecto.

---
+++ TapitasApp/TapitasApp/TapitasApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TapitasApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRepartir_Clicked(object sender, EventArgs e)
+        {
+
+            if(!string.IsNullOrEmpty(txtNumJugadores.Text) && !string.IsNullOrEmpty(txtNumTapas.Text))
+            {
+                var numJugadores = int.Parse(txtNumJugadores.Text);
+                var numTapas = int.Parse(txtNumTapas.Text);
+
+                if(numJugadores <= 1 || numJugadores > 4)
+                {
+                    DisplayAlert("Alerta", "Número de jugadores no valido", "oK");
+                    return;
+                }
+                    Navigation.PushAsync(new Numeros(numJugadores));
+                    txtNumJugadores.Text = "";
+                    txtNumTapas.Text = "";
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Todos los campos son requeridos", "Ok");
+            }
+        }
+    }
+}
---
+++ TapitasApp/TapitasApp/TapitasApp/Numeros.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TapitasApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Numeros : ContentPage
+    {
+        public Numeros(int numJugadores)
+        {
+            InitializeComponent();
+
+
+
+            for(var i = 1; i <= numJugadores; i++)
+            {   
+                Label lbl = new Label();
+                lbl.Text = "Jugador " + i.ToString();
+                lbl.TextColor = Color.Black;
+                lbl.FontAttributes = FontAttributes.Bold;
+
+                Entry entry = new Entry();
+                entry.Placeholder = "321 0000000";
+                entry.Keyboard = Keyboard.Telephone;
+                entry.MaxLength = 10;
+
+                panelTelJugadores.Children.Add(lbl);
+                panelTelJugadores.Children.Add(entry);
+
+            }
+        }
+    }
+}
