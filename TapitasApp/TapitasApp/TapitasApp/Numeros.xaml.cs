76950533b1924e296d613a8f40122d382844ddec
Daniel Mateo Guerra Montoya <dmguerra@correo.iue.edu.co>
2022-12-16 17:26:57 -0500

    Se replantea la generacion de campos dinamicos

--- TapitasApp/TapitasApp/TapitasApp/Numeros.xaml.cs
+++ TapitasApp/TapitasApp/TapitasApp/Numeros.xaml.cs
@@ -14,88 +14,95 @@ namespace TapitasApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Numeros : ContentPage
     {
+
         public Numeros(int numJugadores, int numTapas)
         {
             InitializeComponent();
-            generaCampos(numJugadores);
-
             Jugadores = numJugadores;
             Tapas = numTapas;
+            generaCampos();
         }
 
-        public int Jugadores { get; set; }
+        public static int Jugadores { get; set; }
         public int Tapas { get; set; }
 
-        public void generaCampos(int jugadores)
+        Entry[] campoTexto = new Entry[Jugadores];
+
+        public void generaCampos()
         {
-            for (var i = 1; i <= jugadores; i++)
+
+            for (var i = 0; i < Jugadores; i++)
             {
-                Label lbl = new Label();
-                lbl.Text = "Jugador " + i.ToString();
-                lbl.TextColor = Color.Black;
-                lbl.FontAttributes = FontAttributes.Bold;
+                campoTexto[i] = new Entry();
+                campoTexto[i].Placeholder = $"Jugador {i}";
 
-                Entry entry = new Entry();
-                entry.Placeholder = "321 0000000";
-                entry.Keyboard = Keyboard.Telephone;
-                entry.HorizontalTextAlignment = TextAlignment.Center;
-                entry.MaxLength = 10;
-
-                panelTelJugadores.Children.Add(lbl);
-                panelTelJugadores.Children.Add(entry);
+                panelTelJugadores.Children.Add(campoTexto[i]);
             }
         }
 
-        public int[] numerosTapas(int tapas, int jugadores)
+        public void loquesea(Entry[] campo)
         {
-            int[] numeroTapa = new int[tapas * jugadores];
-
-            //numeros es numeroTapa
-
-
-            //int n;
-
-            //for(var i = 0; i < numeroTapa.Length; i++)
-            //{
-            //    numeroTapa[i] = rdm.Next(1, 16);
-            //}
-
-            Random rdm = new Random();
-            bool repetido;
-            int numero;
-            int indice = 0;
-
-            while (indice < numeroTapa.Length)
+            for(var i = 0; i < campo.Length; i++)
             {
-
-                repetido = false;
-
-                numero = rdm.Next(1, 16);
-
-                for (int i = 0; i < indice; i++)
-                {
-                    if (numeroTapa[i] == numero)
-                    {
-                        repetido = true;
-                    }
-                    //DisplayAlert("Alert", $"Los numeros son {numeroTapa[i]}", "Ok");
-                }
-
-                if (!repetido)
-                {
-                    numeroTapa[indice] = numero;
-                    indice++;
-                    DisplayAlert("Alert", $"Los numeros son {numeroTapa[indice]}", "Ok");
-                }
-                //DisplayAlert("Alert", $"Los numeros son {numeroTapa[i]}", "Ok");
+            DisplayAlert("Alerta", $"{campo[i].Text}", "OK");
             }
-
-            return numeroTapa;
         }
+
+
+        //public void AgregarLabel(StackLayout sl, string name)
+        //{
+        //    Label label = new Label() { Text = name };
+        //    label.TextColor = Color.Black;
+        //    label.FontAttributes = FontAttributes.Bold;
+
+        //    sl.Children.Add(label);
+        //}
+
+        //public void LeerTexto()
+        //{
+        //    foreach(View v in panelTelJugadores.Children)
+        //    {
+        //        if(v.StyleId != null)
+        //        {
+        //            DisplayAlert("Alerta","No puedes estar vacios","Ok");
+        //        }
+        //    }
+        //}
+
+        //public int[] numerosTapas(int tapas, int jugadores)
+        //{
+        //    int[] numeroTapa = new int[tapas * jugadores];
+
+        //    Random rdm = new Random();
+        //    bool repetido;
+        //    int numero;
+        //    int indice = 0;
+
+        //    while (indice < numeroTapa.Length)
+        //    {
+        //        repetido = false;
+        //        numero = rdm.Next(1, 16);
+
+        //        for (int i = 0; i < indice; i++)
+        //        {
+        //            if (numeroTapa[i] == numero)
+        //            {
+        //                repetido = true;
+        //            }
+        //        }
+
+        //        if (!repetido)
+        //        {
+        //            numeroTapa[indice] = numero;
+        //            indice++;
+        //        }
+        //    }
+        //    return numeroTapa;
+        //}
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            numerosTapas(this.Tapas, this.Jugadores);
+            loquesea(campoTexto);
         }
     }
 }
